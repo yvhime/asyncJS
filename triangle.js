a85df1d57594587cad78f641246df9d7388f0999
yvhime <yvhime@gmail.com>
2021-03-17 10:09:47 +0800

    function and arrow

---
+++ triangle.js
@@ -0,0 +1,14 @@
+const a = (base, height) => {
+    const area = (base * height) / 2;
+    // area = ;
+    console.log(area);
+}
+
+a(33, 3);
+
+function triArea(base, height) {
+    const triangleArea = (base * height) / 2;
+    console.log(triangleArea);
+}
+
+triArea(33, 3);
